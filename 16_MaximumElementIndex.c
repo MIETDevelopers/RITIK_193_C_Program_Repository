3a7c43ce042c67ff630f1ff3e80e7c5d998d3d5c
ritik058 <76253497+ritik058@users.noreply.github.com>
2021-03-20 17:00:46 +0530

    MaximumElementIndex file

---
+++ 16_MaximumElementIndex.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+  int i,limit;
+  float arr[limit];
+
+  printf("Please enter limit:\n ");
+  scanf("%d", &limit);
+
+  for (i = 0; i < limit; ++i)
+  {
+    scanf("%f", &arr[i]);
+  }
+
+    for (i = 1; i < limit; ++i)
+    {
+      if (arr[0] < arr[i])
+          arr[0] = arr[i];
+    }
+  printf("Largest element = %.2f", arr[0]);
+  printf("largest index =%d", limit);
+  return 0;
+}
